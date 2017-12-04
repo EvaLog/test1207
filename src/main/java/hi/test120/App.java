75cc3c1b45ed8d0fdc884c705114f36428466f5b
GD <GD@DESKTOP-KJ5655N>
2017-12-04 09:52:03 +0900

    ㅎㅇ

---
+++ src/main/java/hi/test120/App.java
@@ -0,0 +1,14 @@
+package hi.test120;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "떠 버렸다..." );
+    }
+}
